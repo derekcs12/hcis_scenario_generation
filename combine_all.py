111f2405ca650cd7da26a2bb010c7a951a4aacd2
sysnycu <sys.cs12@nycu.edu.tw>
2024-10-09 18:31:15 +0800

    add statistic mode

--- combine_all.py
+++ combine_all.py
@@ -1,60 +1,110 @@
+import argparse
 import glob
 import combine
 import os
 import datetime
 import itertools
 
-# basic scenarios folder
-basic_scenarios_folder = 'scenario_config'
+def combine_all_scenarios(basic_scenarios_folder):
+    # Get all folder in ./scenario_config
+    all_basis = os.listdir(basic_scenarios_folder)
+    print(f"Total {len(all_basis)} folder found.")
 
-# Get all folder in ./scenario_config
-all_basis = os.listdir(basic_scenarios_folder)
-print(f"Total {len(all_basis)} folder found.")
+    # Define save folder
+    # save_root = f'scenario_config_combined/{datetime.datetime.now()}'
+    save_root = f'scenario_config_combined/{datetime.datetime.date(datetime.datetime.now())}'
 
-# Define save folder
-# save_root = f'scenario_config_combined/{datetime.datetime.now()}'
-save_root = f'scenario_config_combined/{datetime.datetime.date(datetime.datetime.now())}'
+    # Create save folder
+    if not os.path.exists(save_root):
+        os.makedirs(save_root)
 
-# Create save folder
-if not os.path.exists(save_root):
-    os.makedirs(save_root)
+    # Combine all yaml and csv files
+    cata_combinations = list(itertools.combinations(all_basis, 2))
+    print("Total combinations: ", len(cata_combinations))
 
-# Combine all yaml and csv files
-cata_combinations = list(itertools.combinations(all_basis, 2))
-print("Total combinations: ", len(cata_combinations))
+    counter = 0
+    for cata1, cata2 in cata_combinations:
+        # get the number of scenarios in each folder
+        cata_1s = len(glob.glob(os.path.join(f'./scenario_config/{cata1}', '*.yaml'), recursive=False))
+        cata_2s = len(glob.glob(os.path.join(f'./scenario_config/{cata2}', '*.yaml'), recursive=False))
 
-counter = 0
-for cata1, cata2 in cata_combinations:
-    # get the number of scenarios in each folder
-    cata_1s = len(glob.glob(os.path.join(f'./scenario_config/{cata1}', '*.yaml'), recursive=False))
-    cata_2s = len(glob.glob(os.path.join(f'./scenario_config/{cata2}', '*.yaml'), recursive=False))
-
-    # get all combinations of scenarios (scenario's index starts from 1)
-    sce_combinations = list(itertools.product(range(1,cata_1s+1), range(1,cata_2s+1)))
-    print(f"Combining {cata1} and {cata2} with {len(sce_combinations)} scenarios.")
-    counter += len(sce_combinations)
-    
-    sc_folder_name = combine.combine_scenario_name(cata1, cata2) 
-    print(f"Combined folder name: {sc_folder_name}")
-    print('-----------------------------------')
-    for sce1, sce2 in sce_combinations:
-        # get the current scenario number in the folder
-        if not os.path.exists(f'{save_root}/{sc_folder_name}'):
-            os.makedirs(f'{save_root}/{sc_folder_name}')
-        scenario_number = len(glob.glob(os.path.join(f'{save_root}/{sc_folder_name}', '*.yaml'), recursive=False))+1
-        combined_scenario_name = f'{sc_folder_name}_{scenario_number}'
+        # get all combinations of scenarios (scenario's index starts from 1)
+        sce_combinations = list(itertools.product(range(1,cata_1s+1), range(1,cata_2s+1)))
+        print(f"Combining {cata1} and {cata2} with {len(sce_combinations)} scenarios.")
+        counter += len(sce_combinations)
         
-        yaml1 = f'{basic_scenarios_folder}/{cata1}/{sce1}.yaml'
-        yaml2 = f'{basic_scenarios_folder}/{cata2}/{sce2}.yaml'
-        combined_yaml= combine.combine_yaml(yaml1, yaml2, combined_scenario_name, mode='agent')
+        sc_folder_name = combine.combine_scenario_name(cata1, cata2) 
+        print(f"Combined folder name: {sc_folder_name}")
+        print('-----------------------------------')
+        for sce1, sce2 in sce_combinations:
+            # get the current scenario number in the folder
+            if not os.path.exists(f'{save_root}/{sc_folder_name}'):
+                os.makedirs(f'{save_root}/{sc_folder_name}')
+            scenario_number = len(glob.glob(os.path.join(f'{save_root}/{sc_folder_name}', '*.yaml'), recursive=False))+1
+            combined_scenario_name = f'{sc_folder_name}_{scenario_number}'
+            
+            yaml1 = f'{basic_scenarios_folder}/{cata1}/{sce1}.yaml'
+            yaml2 = f'{basic_scenarios_folder}/{cata2}/{sce2}.yaml'
+            combined_yaml= combine.combine_yaml(yaml1, yaml2, combined_scenario_name, mode='agent')
 
-        csv1 = f'{basic_scenarios_folder}/{cata1}/{sce1}.csv'
-        csv2 = f'{basic_scenarios_folder}/{cata2}/{sce2}.csv'
-        combined_csv = combine.combine_csv(csv1, csv2, combined_scenario_name, mode='agent')
+            csv1 = f'{basic_scenarios_folder}/{cata1}/{sce1}.csv'
+            csv2 = f'{basic_scenarios_folder}/{cata2}/{sce2}.csv'
+            combined_csv = combine.combine_csv(csv1, csv2, combined_scenario_name, mode='agent')
 
-        # save the combined data
-        save_path = f'{save_root}/{sc_folder_name}/{scenario_number}'
-        combine.save_yaml(combined_yaml, f'{save_path}.yaml')
-        combine.save_csv(combined_csv, f'{save_path}.csv')
+            # save the combined data
+            save_path = f'{save_root}/{sc_folder_name}/{scenario_number}'
+            combine.save_yaml(combined_yaml, f'{save_path}.yaml')
+            combine.save_csv(combined_csv, f'{save_path}.csv')
 
-print(f"{counter} scenarios combined.")
+    print(f"{counter} scenarios combined.")
+
+def show_statistics(basic_scenarios_folder):
+    # Get all folder in ./scenario_config
+    all_basis = os.listdir(basic_scenarios_folder)
+    print(f"Total {len(all_basis)} folder found.")
+
+    # Combine all yaml and csv files
+    cata_combinations = list(itertools.combinations(all_basis, 2))
+    print("Total combinations: ", len(cata_combinations))
+
+    counter = 0
+    for cata1, cata2 in cata_combinations:
+        # get the number of scenarios in each folder
+        cata_1s = len(glob.glob(os.path.join(f'./scenario_config/{cata1}', '*.yaml'), recursive=False))
+        cata_2s = len(glob.glob(os.path.join(f'./scenario_config/{cata2}', '*.yaml'), recursive=False))
+
+        # get all combinations of scenarios (scenario's index starts from 1)
+        sce_combinations = list(itertools.product(range(1,cata_1s+1), range(1,cata_2s+1)))
+        print(f"[ {cata1} ] x [ {cata2} ] =  {cata_1s} x {cata_2s}  = {len(sce_combinations)} SCs")
+        counter += len(sce_combinations)
+        
+    print(f"Total {counter} scenarios.")
+
+
+
+if __name__ == '__main__':
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument(
+        '-m', '--mode',
+        metavar='MODE',
+        default='combine',
+        type=str,
+        help='statistic/s or combine/c')
+    argparser.add_argument(
+        '-f', '--folder',
+        metavar='FOLDER',
+        default='scenario_config',
+        type=str,
+        help='basic scenario folder')
+    
+    args = argparser.parse_args()
+
+    if args.mode == 'combine' or args.mode == 'c':
+        combine_all_scenarios(args.folder)
+    elif args.mode == 'statistic' or args.mode == 's':
+        show_statistics(args.folder)
+
+
+
+
+
