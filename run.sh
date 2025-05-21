74da3ec1a5f2957a9ca128e4f057bcf38548e473
derekwuchengyu <40729507+derekwuchengyu@users.noreply.github.com>
2025-05-21 21:04:38 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,12 @@
-# 生成Scenario config files(yaml), csvs and xoscs
+# 生成Scenario config files(.yaml), csvs and xoscs
+# Generation config files(.yaml & .csv)
 python config_gen_all.py
+
+# Do Config Files Combination (both .yaml & .csv)
 python combine_all.py
+
+# Turn .yaml into .xosc
 python main.py -c all
+
+# Upload .xosc with csv's paramerter ranges to itri payload
+python scenario_upload.py -s all
